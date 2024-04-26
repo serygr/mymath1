616a91cd9d9792d309d0842ab526a4e2de31b943
serygr <serygr94@gmail.com>
2024-04-26 05:20:44 +0500

    task1.12.4.1

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package mymath
 
 import "math"
 
-
-
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
@@ -27,4 +25,14 @@ func Abs(x float64) float64 {
 }
 func Yn(n int, x float64) float64 {
 	return math.Yn(n, x)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
 }
